62050b6974cf0ad1c836bda0a340cc977242d0d3
Amit Kumar Dubey <95951027+Amit-Kr-Dubey@users.noreply.github.com>
2022-09-05 23:12:59 +0530

    Create Strings.cpp

---
+++ Strings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+  
+  string a, b;
+    cin >> a >> b;
+    cout << a.length() << " " << b.length() << endl;
+    cout << a << b << endl;
+    swap(a[0], b[0]);
+    cout << a << " " << b << endl;
+    
+    return 0;
+}
